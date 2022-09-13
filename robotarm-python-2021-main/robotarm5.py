05810e5584532024667d3c3689712a6e63517a70
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-13 09:36:51 +0200

    robotarms

---
+++ robotarm-python-2021-main/robotarm5.py
@@ -0,0 +1,11 @@
+from RobotArm import RobotArm
+            
+# De robotarm opstarten in een bepaald level, bijvoorbeeld 'exercise 1'
+robotArm = RobotArm('exercise 5')
+            
+# Jouw python instructies zet je vanaf hier:
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
