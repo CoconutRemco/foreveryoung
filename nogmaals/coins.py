d8b77c4ffa9cb5ba05d1be8488b3b44e2dc1aaee
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-10-31 14:13:29 +0100

    FIxes

--- nogmaals/coins.py
+++ nogmaals/coins.py
@@ -8,6 +8,16 @@ toPay = int(float(input('Amount to pay: '))* 100) #this makes it easier for prog
 paid = int(float(input('Paid amount: ')) * 100) #makes it easier for program to understand
 change = paid - toPay #this wil see how many you have to get back
 
+cr500=0
+cr300=0
+cr200=0
+cr50=0
+cr20=0
+cr10=0
+cr5=0
+cr2=0
+cr1=0
+
 if change > 5: #if the change is bigger than 5 you get paid in 5s
   coinValue = 500 #this wil see if you get paid in 5s or 3s
 elif change > 3:
@@ -30,25 +40,36 @@ while change > 0 and coinValue > 0: #This will make sure you have to return coin
 
 # comment on code below:If the value is the same as 1 of the coins you will get paid in the coins with less value 
     if coinValue == 500:
+      cr500=nrCoinsReturned
       coinValue = 300
     elif coinValue == 300:
+      cr300=nrCoinsReturned
       coinValue = 200
     elif coinValue == 200:
+      cr200=nrCoinsReturned
       coinValue = 50
     elif coinValue == 50:
+      cr50=nrCoinsReturned
       coinValue = 20
     elif coinValue == 20:
+      cr20=nrCoinsReturned
       coinValue = 10
     elif coinValue == 10:
+      cr10=nrCoinsReturned
       coinValue = 5
     elif coinValue == 5:
+      cr5=nrCoinsReturned
       coinValue = 2
     elif coinValue == 2:
+      cr2=nrCoinsReturned
       coinValue = 1
     else:
+      cr1=nrCoinsReturned
       coinValue = 0
 
 if change > 0: #this wil make sure you have returned the correct amount
   print('Change not returned: ', str(change) + ' cents')
 else:
   print('done')
+
+  print("5 euro muntstukken "+ str(cr50))
--- robotarm-python-2021-main/robotarm9.py
+++ robotarm-python-2021-main/robotarm9.py
@@ -3,18 +3,14 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 9')
 
 # Jouw python instructies zet je vanaf hier:
-for i in range(4):
-    for i in range(4):
+for x in range(1,5):
+    for s in range(x):
         robotArm.grab()
         for i in range(5):
             robotArm.moveRight()
         robotArm.drop()
-        for i in range(4):
+        for a in range(5):
             robotArm.moveLeft()
-    for i in range(4):
-        robotArm.moveLeft()
-
-
-
+    robotArm.moveRight()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
